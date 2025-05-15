6829b6a5ed80e57dd7e1b3454ddd8679f0bc0f58
kimchid <149261601+kimchid@users.noreply.github.com>
2025-05-15 22:57:20 +0200

    neu

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function navigateTo(url) {
-    window.location.href = url;
-  }
-  
+function redirectToRandomPage() {
+  const pages = [
+    "seite1/index.html",
+    "seite2/index.html",
+    "seite3/index.html"
+  ];
+
+  const randomPage = pages[Math.floor(Math.random() * pages.length)];
+  window.location.href = randomPage;
+}
+
+setTimeout(redirectToRandomPage, 1000); // 1 Sekunde warten
